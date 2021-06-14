556214f15e91d672d333d2d8ee9cdd3ff40234a5
talha <abutalhasiam1998@gmail.com>
2021-06-14 20:51:23 +0600

    Convert in to theme

---
+++ functions.php
@@ -0,0 +1,256 @@
+<?php
+/**
+ * gympro functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package gympro
+ */
+
+if ( ! defined( '_S_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.0' );
+}
+
+if ( ! function_exists( 'gympro_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function gympro_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on gympro, use a find and replace
+		 * to change 'gympro' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'gympro', get_template_directory() . '/languages' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus(
+			array(
+				'menu-1' => esc_html__( 'Primary', 'gympro' ),
+			)
+		);
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+				'style',
+				'script',
+			)
+		);
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support(
+			'custom-background',
+			apply_filters(
+				'gympro_custom_background_args',
+				array(
+					'default-color' => 'ffffff',
+					'default-image' => '',
+				)
+			)
+		);
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support(
+			'custom-logo',
+			array(
+				'height'      => 250,
+				'width'       => 250,
+				'flex-width'  => true,
+				'flex-height' => true,
+			)
+		);
+	}
+endif;
+add_action( 'after_setup_theme', 'gympro_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function gympro_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'gympro_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'gympro_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function gympro_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'gympro' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'gympro' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'gympro_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function gympro_scripts() {
+	wp_enqueue_style( 'gympro-style', get_stylesheet_uri(), array(), _S_VERSION );
+
+	wp_enqueue_style( 'favicon', get_template_directory_uri()."/img/favicon.ico", array(), _S_VERSION );
+
+
+		/*
+	
+	     ............. CSS Here...............
+	
+	*/
+
+
+	wp_enqueue_style( 'bootstrap',"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), _S_VERSION );
+
+	wp_enqueue_style( 'allmin',"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css", array(), _S_VERSION );
+
+	wp_enqueue_style( 'animat', get_template_directory_uri()."/lib/animate/animate.min.css", array(), _S_VERSION );
+
+	wp_enqueue_style( 'flaticon', get_template_directory_uri()."/lib/flaticon/font/flaticon.css", array(), _S_VERSION );
+
+
+	wp_enqueue_style( 'owl', get_template_directory_uri()."/lib/owlcarousel/assets/owl.carousel.min.css", array(), _S_VERSION );
+
+
+	wp_enqueue_style( 'lightbox', get_template_directory_uri()."/lib/lightbox/css/lightbox.min.css", array(), _S_VERSION );
+
+
+	wp_enqueue_style( 'maincss', get_template_directory_uri()."/css/style.css", array(), _S_VERSION );
+
+
+
+
+	/*
+	
+	     ............. JS Here...............
+	
+	*/
+
+
+
+	wp_enqueue_script( 'jquery',"https://code.jquery.com/jquery-3.4.1.min.js", array(), _S_VERSION );
+
+
+	wp_enqueue_script( 'boot_bundel',"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js", array(), _S_VERSION );
+
+
+	wp_enqueue_script( 'easing_min',get_template_directory_uri()."/lib/easing/easing.min.js", array(), _S_VERSION );
+
+
+	wp_enqueue_script( 'wow_min',get_template_directory_uri()."/lib/wow/wow.min.js", array(), _S_VERSION );
+
+	wp_enqueue_script( 'owl_carousel',get_template_directory_uri()."/lib/owlcarousel/owl.carousel.min.js", array(), _S_VERSION );
+
+
+	wp_enqueue_script( 'isotope_min',get_template_directory_uri()."/lib/isotope/isotope.pkgd.min.js", array(), _S_VERSION );
+
+
+	wp_enqueue_script( 'lightbox_js',get_template_directory_uri()."/lib/lightbox/js/lightbox.min.js", array(), _S_VERSION );
+
+
+	wp_enqueue_script( 'bootvalidation',get_template_directory_uri()."/mail/jqBootstrapValidation.min.js", array(), _S_VERSION );
+
+	wp_enqueue_script( 'contact',get_template_directory_uri()."/mail/contact.js", array(), _S_VERSION );
+
+
+	wp_enqueue_script( 'main_js',get_template_directory_uri()."/js/main.js", array(), _S_VERSION );
+
+
+
+
+
+
+
+
+	
+
+
+	wp_style_add_data( 'gympro-style', 'rtl', 'replace' );
+
+	wp_enqueue_script( 'gympro-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'gympro_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
+
